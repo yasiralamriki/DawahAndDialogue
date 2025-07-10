34863af904d0711a088e2d8aa5a9ca32757bc8d4
ياسر الأمريكي <70416702+yasiralamriki@users.noreply.github.com>
2025-07-10 15:54:47 -0400

    refactor(commands): enhance date command to use Intl.DateTimeFormat for improved date and time handling

--- commands/utility/date.js
+++ commands/utility/date.js
@@ -18,29 +18,45 @@ export default {
                 .setRequired(false) // Optional option
         ),
     async execute(interaction) {
+        // Command options
+        const timezone = 'UTC'; // Default to UTC if no timezone is provided
+        const showGregorian = interaction.options.getBoolean('gregorian') || true; // Default to true if not provided
+        const is24HourTime = interaction.options.getBoolean('24_hour_time') || false; // Default to false if not provided
+
+        // Create locales and formats for date and time
+        // Using Intl.Locale and Intl.DateTimeFormat for better date handling
+        const gregorianLocale = new Intl.Locale('en', {
+            region: 'US',
+            calendar: 'gregory',
+            timeZone: timezone,
+        });
+
+        const hijriLocale = new Intl.Locale('en', {
+            calendar: 'islamic',
+            timeZone: timezone,
+        });
+
+        const dateFormat = new Intl.DateTimeFormat(gregorianLocale, {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+            hour12: is24HourTime
+        });
+
+        const timeFormat = new Intl.DateTimeFormat(gregorianLocale, {
+            hour: 'numeric',
+            minute: 'numeric',
+            second: 'numeric',
+            hour12: is24HourTime
+        });
+
         // Get current date
         const date = new Date();
 
-        // Gregorian date (formatted)
-        const gregorianDate = date.toLocaleDateString('en-US', {
-            year: 'numeric',
-            month: 'long',
-            day: 'numeric'
-        });
-
-        // Hijri date (formatted)
-        const hijriDate = date.toLocaleDateString('en-TN-u-ca-islamic', {
-            year: 'numeric',
-            month: 'long',
-            day: 'numeric'
-        });
-
-        // Time
-        const time = date.toLocaleTimeString('en-US', {
-            hour: '2-digit',
-            minute: '2-digit',
-            hour12: interaction.options.getBoolean('24_hour_time') !== true // Check if 24-hour format is requested
-        });
+        // Get date & time
+        const gregorianDate = date.toLocaleDateString(gregorianLocale, dateFormat);
+        const hijriDate = date.toLocaleDateString(hijriLocale, dateFormat);
+        const time = date.toLocaleTimeString(gregorianLocale, timeFormat);
 
         // Create an embed with the current date and time
         const dateEmbed = new EmbedBuilder()
